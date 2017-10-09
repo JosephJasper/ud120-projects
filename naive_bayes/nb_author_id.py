463db5027bc1aa812c8b79b92647146ef1945a35
JosephJasper <Joseph.Jasper@gmail.com>
2017-10-08 19:07:36 -0700

    NB part 1
    Added classify function for NB and test accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,7 +27,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def classify(features_train,labels_train):
+	clf = GaussianNB()
+	clf.fit(features_train,labels_train)
+	return clf
 
+clf = classify(features_train,labels_train)
+pred = clf.predict(features_test)
+accuracy = accuracy_score(labels_test,pred)
+
+print accuracy
 
 #########################################################
 
