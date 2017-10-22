e6dea681dce88cc6bab9be57989c9225301e0474
JosephJasper <Joseph.Jasper@gmail.com>
2017-10-22 15:38:53 -0700

    Decision Tree module
    Created code for basic decision tree algorithm.
    Modified the feature selector to reduce the number of features used in
    that decision tree.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,7 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print len(features_train[0])
 
+def classify(features_train,labels_train):
+	clf = tree.DecisionTreeClassifier(min_samples_split=40)
+	clf.fit(features_train,labels_train)
+	return clf
+
+t0 = time()
+clf = classify(features_train,labels_train)
+print "Training time:", round(time()-t0,3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "Predicting time:", round(time()-t1,3), "s"
+accuracy = accuracy_score(labels_test,pred)
+
+print accuracy
 #########################################################
 
 
