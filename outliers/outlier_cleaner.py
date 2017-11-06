2de876940f10e7f0e1b28f09d647e3910ea09841
JosephJasper <Joseph.Jasper@gmail.com>
2017-11-05 16:41:41 -0800

    Lesson 8: Outliers Part 1
    Created regression for Age vs Net Worth data set.
    Added script to outlier_cleaner function to remove 10% of the data as
    outliers.

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,20 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    outliers = []
+    data_Length = max(len(predictions),len(ages),len(net_worths))
+    for a in range(0,data_Length):
+        cleaned_data.append((ages[a],net_worths[a],net_worths[a]-predictions[a]))
 
-    
+    while len(cleaned_data) > (.9 * data_Length):
+        cleaned_Data_Length = len(cleaned_data)
+        error_Max = 0
+        outlier_Row = 0
+        for a in range(0,cleaned_Data_Length):
+            if abs(cleaned_data[a][2]) > error_Max:
+                error_Max = abs(cleaned_data[a][2])
+                outlier_Row = a
+        outliers.append(cleaned_data.pop(outlier_Row))
+
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,13 +25,19 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+reg = LinearRegression()
+reg.fit(ages_train,net_worths_train)
 
+coef = reg.coef_
+coef_Text = ""
+for a in range(0,len(coef)):
+    coef_Text += str(coef[a]) + "x" + str(a+1) + " +"
+print "y = ", coef_Text,reg.intercept_
 
-
-
-
-
-
+print "r2 of train data: ",r2_score(net_worths_train,reg.predict(ages_train))
+print "r2 of test data: ",r2_score(net_worths_test,reg.predict(ages_test))
 
 
 
@@ -42,7 +48,6 @@ except NameError:
     pass
 plt.scatter(ages, net_worths)
 plt.show()
-
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
@@ -69,6 +74,15 @@ if len(cleaned_data) > 0:
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        coef = reg.coef_
+        coef_Text = ""
+        for a in range(0,len(coef)):
+            coef_Text += str(coef[a]) + "x" + str(a+1) + " +"
+        print "cleaned y = ", coef_Text,reg.intercept_
+
+        print "new r2 of cleaned data: ",r2_score(net_worths,reg.predict(ages))
+        print "new r2 of test data: ",r2_score(net_worths_test,reg.predict(ages_test))
+
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
