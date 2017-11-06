0ffde536eb2848fd122da47afa4d49b9c093801d
JosephJasper <Joseph.Jasper@gmail.com>
2017-11-05 16:47:14 -0800

    Lesson 8: Outliers Part 2
    Evaluated Enron data for potential outliers.
    Identified Total as an invalid data point, but Ken Lay and Jeffrey
    Skilling as valid/important data points.

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,6 +9,7 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop("TOTAL")
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
@@ -16,4 +17,17 @@ data = featureFormat(data_dict, features)
 ### your code below
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+
+print "Highest Salary and Bonus"
+for a in data_dict:
+	if data_dict[a]['salary'] != "NaN" and data_dict[a]['bonus'] != "NaN":
+		if data_dict[a]['salary'] > 1000000 and data_dict[a]['bonus'] > 5000000:
+			print "Employee:", a, " Salary:", data_dict[a]['salary'], " Bonus:", data_dict[a]['bonus']
